9b08c13b048b0fd3e481694c32efb29b4711c215
Ali <133884883+nazarovqurbonali@users.noreply.github.com>
2024-04-13 17:35:00 +0500

    v-3

--- Domain/Entities/Course.cs
+++ Domain/Entities/Course.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Domain.Entities;
 
 public class Course
 {
+    [Key]
     public int Id { get; set; }
-    public required string CourseName { get; set; }
+
+    [Required,MaxLength(100)]
+    public required  string CourseName { get; set; }
     public List<Group>? Groups { get; set; }
 }
--- Infrastructure/Data/DataContext.cs
+++ Infrastructure/Data/DataContext.cs
@@ -15,4 +15,42 @@ public class DataContext:DbContext
     public DbSet<StudentAddress> StudentAddresses { get; set; }
     public DbSet<StudentGroup> StudentGroups { get; set; }
     public DbSet<Student> Students { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        
+        #region one to many
+
+        modelBuilder.Entity<Course>().HasMany(x => x.Groups)
+            .WithOne(x => x.Course)
+            .HasForeignKey(x => x.CourseId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        #endregion
+
+        #region One to one
+
+        modelBuilder.Entity<StudentAddress>()
+            .HasOne(x => x.Student)
+            .WithOne(x => x.StudentAddress);
+
+        #endregion
+
+        #region Many to Many
+
+        modelBuilder.Entity<StudentGroup>()
+            .HasOne(x => x.Student)
+            .WithMany(x => x.StudentGroups);
+        
+        
+        modelBuilder.Entity<StudentGroup>()
+            .HasOne(x => x.Group)
+            .WithMany(x => x.StudentGroups);
+
+        modelBuilder.Entity<StudentGroup>().HasKey(x => new { x.StudentId, x.GroupId });
+
+        #endregion
+        
+        base.OnModelCreating(modelBuilder);
+    }
 }
